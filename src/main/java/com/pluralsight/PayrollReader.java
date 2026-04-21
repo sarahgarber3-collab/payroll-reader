3dfba359acfde8d898bc9e71f6cd1dfe8e87cc66
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-04-21 15:28:48 -0400

    constructor, getters and setters

--- src/main/java/com/pluralsight/PayrollReader.java
+++ src/main/java/com/pluralsight/PayrollReader.java
@@ -1,7 +1,32 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class PayrollReader {
     public static void main(String[] args) {
+        try {
+            FileReader fileReader=new FileReader("src/main/resources/employee.csv");
+            BufferedReader bufReader = new BufferedReader(fileReader);
+
+            String input;
+
+            while((input = bufReader.readLine()) != null) {
+                System.out.println(input);
+                try {
+                    Thread.sleep(1000); //pauses program, for ms time
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            bufReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
