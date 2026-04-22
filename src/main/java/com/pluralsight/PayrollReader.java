b03e212821a71117c0141f82506a470b44c16516
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-04-22 10:05:17 -0400

    splitting, parsing, and throwing away the first line

--- src/main/java/com/pluralsight/PayrollReader.java
+++ src/main/java/com/pluralsight/PayrollReader.java
@@ -1,7 +1,6 @@
 package com.pluralsight;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -12,9 +11,18 @@ public class PayrollReader {
             BufferedReader bufReader = new BufferedReader(fileReader);
 
             String input;
+            bufReader.readLine();
 
             while((input = bufReader.readLine()) != null) {
-                System.out.println(input);
+                String[] parts = input.split("\\|");
+                int employeeId = Integer.parseInt(parts[0]);
+                String name = parts[1];
+                float hoursWorked = Float.parseFloat(parts[2]);
+                float payRate = Float.parseFloat(parts[3]);
+
+                Employee emp = new Employee(employeeId, name, hoursWorked, payRate);
+                System.out.println(emp.getName() + " - Gross Pay: $" + emp.getGrossPay());
+
                 try {
                     Thread.sleep(600); //pauses program, for ms time
                 } catch (InterruptedException e) {
