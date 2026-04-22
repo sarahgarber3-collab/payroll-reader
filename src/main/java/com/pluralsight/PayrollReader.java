f3309a69492890b7cf240237166088737412db83
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-04-22 13:54:38 -0400

    writer lines progress

--- src/main/java/com/pluralsight/PayrollReader.java
+++ src/main/java/com/pluralsight/PayrollReader.java
@@ -1,16 +1,27 @@
 package com.pluralsight;
-
+import java.io.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class PayrollReader {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("--Welcome to file Writer--");
+        System.out.print("Enter the name of the file employee file to process: ");
+        String fileName = scan.nextLine();
+        System.out.print("Enter the name of the payroll file to create: ");
+        String payrollName = scan.nextLine();
+
+
         try {
             FileReader fileReader=new FileReader("src/main/resources/employee.csv");
             BufferedReader bufReader = new BufferedReader(fileReader);
+            BufferedWriter bufWriter = new BufferedWriter(new FileWriter("src/main/resources/employeePay.csv"));
 
             String input;
+            String text;
             bufReader.readLine();
 
             while((input = bufReader.readLine()) != null) {
@@ -21,16 +32,18 @@ public class PayrollReader {
                 float payRate = Float.parseFloat(parts[3]);
 
                 Employee emp = new Employee(employeeId, name, hoursWorked, payRate);
-                System.out.printf("id:%d Name:%s, Gross Pay:%f %n" , emp.getEmployeeId() ,emp.getName(), emp.getGrossPay());
+                System.out.printf("id:%d Name:%s, Gross Pay:$%.2f%n" , emp.getEmployeeId() ,emp.getName(), emp.getGrossPay());
 
-                try {
-                    Thread.sleep(600); //pauses program, for ms time
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+
+                bufWriter.write("%d | %s | $%.2f%n",fileName,payrollName );
             }
 
+
+
+
+
             bufReader.close();
+            bufWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
